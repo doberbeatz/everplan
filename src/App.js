248bf60c5a9ff44cc4abac48af567d4111b754d4
Roman Reprintsev <doberbeatz@gmail.com>
2017-10-21 21:08:33 +0300

    Init react and routers

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import {
+    BrowserRouter as Router,
+    Route
+} from 'react-router-dom'
+import FeedPage from "./pages/FeedPage";
+import LoginPage from "./pages/LoginPage";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Route exact path="/" component={FeedPage}/>
+                    <Route path="/login" component={LoginPage}/>
+                </div>
+            </Router>
+        )
+    }
 }
 
-export default App;
+export default App
---
+++ src/components/NavbarTop.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Menu, Container, Dropdown } from 'semantic-ui-react'
+
+const NavbarTop = () => (
+    <div  style={{ marginBottom: '5em' }}>
+        <Menu fixed='top' inverted>
+            <Container>
+                <Menu.Item as='a' header>Everplan</Menu.Item>
+
+                <Menu.Menu position="right">
+                    <Menu.Item as='a'>Calendar</Menu.Item>
+                    <Dropdown item simple text='Profile'>
+                        <Dropdown.Menu>
+                            <Dropdown.Item>Contacts</Dropdown.Item>
+                            <Dropdown.Item>Settings</Dropdown.Item>
+                            <Dropdown.Divider/>
+                            <Dropdown.Item>Quit</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Menu.Menu>
+            </Container>
+        </Menu>
+    </div>
+);
+
+export default NavbarTop
